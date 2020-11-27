a9a7bbe7aaaa0c4dbe6ee759c3bed29b26a4da57
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-27 14:07:06 +0300

    modified start file

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 SECRET_KEY=342s(s(!hsjd998sde8$=o4$3m!(o+kce2^97kp6#ujhi
-DEBUG=False
+DEBUG=True
 DB_NAME=d98rrb55h2lt3f
 DB_USER=uurluzvvdjelkl
 DB_PASSWORD=4a4166a33efa37450068223737b6ae114bba2e514cd2afd99bf32d49464c0519
